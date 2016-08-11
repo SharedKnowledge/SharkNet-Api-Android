160daa2ff2bc60034d0b2ebf60eed4304a945ff0
Michael Schwarz <schwarzm90@gmail.com>
2016-08-11 10:17:34 +0200

    dies und das.

---
+++ library/src/main/java/net/sharksystem/sharknet_api_android/utils/ContentConverter.java
@@ -0,0 +1,14 @@
+package net.sharksystem.sharknet_api_android.utils;
+
+import java.io.File;
+
+/**
+ * Created by j4rvis on 11.08.16.
+ */
+public class ContentConverter {
+
+    // Convert the content to the File-type described in mimeType
+    static final File content2Type(){
+        return null;
+    }
+}
---
+++ library/src/test/java/net/sharksystem/sharknet_api_android/BaseTest.java
@@ -0,0 +1,16 @@
+package net.sharksystem.sharknet_api_android;
+
+import org.junit.Before;
+
+/**
+ * Created by j4rvis on 11.08.16.
+ */
+public class BaseTest {
+
+    @Before
+    public void init(){
+
+    }
+
+
+}
